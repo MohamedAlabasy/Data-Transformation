51fd90eee213617ed4b3c0e1214244701623a066
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2024-04-18 01:43:21 +0200

    remove AppService & AppController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BrandModule } from './brand/brand.module';
 
 @Module({
-  imports: [BrandModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BrandModule]
 })
-export class AppModule {}
+
+export class AppModule { }

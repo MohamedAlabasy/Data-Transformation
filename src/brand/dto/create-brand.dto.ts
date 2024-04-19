d0ce4559d070b3711bcb0c5dc1eca0b943c4a08a
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2024-04-19 20:05:28 +0200

    update CreateBrandDto

--- src/brand/dto/create-brand.dto.ts
+++ src/brand/dto/create-brand.dto.ts
@@ -1,1 +1,22 @@
-export class CreateBrandDto { }
+import { IsNotEmpty, IsNumber, IsString, Max, Min } from "class-validator";
+
+export class CreateBrandDto {
+    @IsString()
+    @IsNotEmpty({ message: 'Brand name is required' })
+    readonly brandName: string;
+
+    @Min(1600, { message: 'Year founded seems too old' })
+    @Max(new Date().getFullYear(), { message: 'Year founded cannot be in the future' })
+    @IsNumber()
+    @IsNotEmpty({ message: 'Year founded is required' })
+    readonly yearFounded: number;
+
+    @IsString()
+    @IsNotEmpty({ message: 'Headquarters location is required' })
+    readonly headquarters: string;
+
+    @Min(1, { message: 'There should be at least one location' })
+    @IsNumber()
+    @IsNotEmpty()
+    readonly numberOfLocations: number;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new ConfigService();
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    methods: 'GET,POST,PATCH,DELETE',
+    origin: '*'
+  })
+
   await app.listen(config.get('PORT'));
 }
+
 bootstrap();

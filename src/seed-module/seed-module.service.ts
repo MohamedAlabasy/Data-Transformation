3e4ac651da4db784ff440eea33f8987c0bf28313
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2024-04-19 20:41:58 +0200

    using faker with seed

--- src/seed-module/seed-module.service.ts
+++ src/seed-module/seed-module.service.ts
@@ -1,28 +1,27 @@
 import { Command } from 'nestjs-command';
-import { Injectable, Logger } from '@nestjs/common';
+import { faker } from '@faker-js/faker';
+import { Injectable } from '@nestjs/common';
 import { BrandService } from 'src/brand/brand.service';
 import { CreateBrandDto } from 'src/brand/dto/create-brand.dto';
 
 @Injectable()
 export class SeedModuleService {
-  private readonly logger = new Logger(SeedModuleService.name);
-
   constructor(private readonly brandService: BrandService) { }
 
-  @Command({ command: 'create:brand', describe: 'create a brand' })
+  @Command({ command: 'create:brand', describe: 'create 10 brands' })
   async create() {
-    const createBrandDto: CreateBrandDto[] = [];
-    for (let i = 0; i < 10; i++) {
-      createBrandDto.push({
-        brandName: 'brandName',
-        yearFounded: 1600,
-        headquarters: 'headquarters',
-        numberOfLocations: 10,
-      })
-    }
-
+    const createBrandDto: CreateBrandDto[] = faker.helpers.multiple(this.createRandomBrand, { count: 10 });
     const brand = await this.brandService.brandModel.insertMany(createBrandDto);
     console.log(brand);
-    this.logger.log('10 brands created successfully')
+  }
+
+
+  private createRandomBrand(): CreateBrandDto {
+    return {
+      brandName: faker.internet.userName(),
+      yearFounded: faker.number.int({ min: 1600, max: new Date().getFullYear() }),
+      headquarters: faker.internet.userName(),
+      numberOfLocations: faker.number.int({ min: 1 }),
+    }
   }
 }
